2654901b44a89aa9f1773fe1485e28c9bc0c29bd
Mrigank Badola <badola3@gmail.com>
2020-10-10 20:32:23 +0530

    automated games section with igdb api

--- routes/games.js
+++ routes/games.js
@@ -1,29 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../db')
 
+const spawn = require("child_process").spawn;
 router.get('/', function(req, res, next) {
-    db.getalldata('g').then((games)=>
-        db.getalldata('gg').then((game_genres)=>
-            db.getalldata('p').then((platforms)=>
-                res.render('games', {games, game_genres,platforms, title: "Games"}
-                )
-            )
-        )
-    ).catch((err)=>{
-            res.send(err)
-        })
-});
-router.post('/', function(req, res, next) {
-    var game = req.body
-    db.insertgame(game.name, game.year, game.platform, game.series, game.played)
-    db.insertsingle('platforms',game.platform)
-    if(game.genre.size!==1)
-        for (x of game.genre)
-            db.insertgenregame(game.name, x)
-    else
-        db.insertgenregame(game.name, game.genre)
-    res.redirect('games')
+    var prog = spawn('python3',['game.py'])
+    prog.stdout.on('data', function (data){
+        const games = JSON.parse(data);
+        res.render('games', {games, title: "Games"})
+    });
 });
 
 module.exports = router;
--- routes/saveddit.js
+++ routes/saveddit.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 const spawn = require("child_process").spawn;
 router.get('/', function (req, res, next) {
-    var prog = spawn('python3',['main.py'])
+    var prog = spawn('python3',['reddit.py'])
     prog.stdout.on('data', function (data){
         const savedposts = JSON.parse(data);
         res.render('saveddit', {savedposts, title: "Saved Reddit Posts"})
